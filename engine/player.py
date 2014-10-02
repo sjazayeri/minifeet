f15f03be0d8fb6728098a9f893965174ebd60d74
Mohammad Sadegh Jazayeri <sjazayeri404@gmail.com>
2014-10-02 18:58:40 +0330

    more structure and a little functionality

--- engine/player.py
+++ engine/player.py
@@ -1,11 +1,18 @@
 import subprocess as sp
+import select
 import state
 
 def move_to(x, y):
     pass
 
+def kick(x, y):
+    pass
+
+def translate(cmd):
+    pass
+
 class Player:
-    """Player(program_path) -> create object to run
+    """Player(program_path, team, number) -> create object to run
     and interface with player program"""
     def __init__(self, ppath, team, number):
         self.team = team
@@ -13,6 +20,8 @@ class Player:
         self.cmd_list = []
         self.process = sp.Popen(ppath, stdin=sp.PIPE, stdout=sp.PIPE)
         self.process.stdin.write(team+' '+number+'\n')
+        self.poller = select.poll()
+        self.poller.register(self.process.stdout, select.POLLIN)
         
     def terminate(self):
         self.process.terminate()
@@ -22,5 +31,8 @@ class Player:
         pass
 
     def get_command(self):
-        """returns the command for the current cycle as a string""" 
-        pass
+        """returns the command for the current cycle as a string"""
+        buffer_content = []
+        while self.poller.poll(0):
+            buffer_content += self.process.read(1)
+        command = ''.join(buffer_content)
