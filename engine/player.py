c25669797662da4830101b7508329feb259dc64a
Mohammad Sadegh Jazayeri <sjazayeri404@gmail.com>
2014-10-02 17:33:36 +0330

    had saved to wrong file

--- engine/player.py
+++ engine/player.py
@@ -1,15 +1,26 @@
-class Player:
-    """Proxy(program_path) -> create proxy to run
-    and interface with program"""
-    def __init__(self, ppath):
-        pass
+import subprocess as sp
+import state
 
+def move_to(x, y):
+    pass
+
+class Player:
+    """Player(program_path) -> create object to run
+    and interface with player program"""
+    def __init__(self, ppath, team, number):
+        self.team = team
+        self.number = number
+        self.cmd_list = []
+        self.process = sp.Popen(ppath, stdin=sp.PIPE, stdout=sp.PIPE)
+        self.process.stdin.write(team+' '+number+'\n')
+        
+    def terminate(self):
+        self.process.terminate()
+        
     def send_state(self, state):
         """sends the current state to the player"""
         pass
 
     def get_command(self):
-        """returns the command for the current cycle""" 
+        """returns the command for the current cycle as a string""" 
         pass
-
-    
