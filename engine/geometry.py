80872bce0f6f7ce6f977ba18093fed79fa82e16c
Mohammad Sadegh Jazayeri <sjazayeri404@gmail.com>
2014-10-10 16:28:38 +0330

    more geometry

--- engine/geometry.py
+++ engine/geometry.py
@@ -2,8 +2,25 @@
 calculations"""
 
 class Vector:
-    """A point on the cartesian plane.
-    Point(x, y) -> Point referring to (x, y) on the plane"""
+    """A vector in the cartesian plane
+    Vector(x, y) -> creates a vector
+    """
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        
+    def __add__(self, u):
+        return Vector(self.x+u.x, self.y+u.y)
+
+    def __iadd__(self, u):
+        self.x += u.x
+        self.y += u.y
+        return self
+
+    def __mul__(self, s):
+        return Vector(s*self.x, s*self.y)
+
+    def __imul__(self, s):
+        self.x *= s
+        self.y *= s
+        return self
