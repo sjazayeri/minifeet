95e7dd0052f25005f72824500a005d418fe219f2
Nahal <nahal@nahal-VPCCW1CGX.(none)>
2014-10-11 20:53:05 +0330

    done

--- engine/objects.py
+++ engine/objects.py
@@ -2,11 +2,18 @@ from collections import defaultdict
 from geometry import Vector
 import subprocess as sp
 import select
-import state
 
 max_read = 1024 #this is likely to change as soon as the state specs are in
 # test
-class Player:
+
+class MovingObj:
+	def __init__ (self , pos=Vector(0, 0) , vel = Vector(0, 0) ,   ground=None) :
+		self.ground = ground
+	def move (self) :
+		raise NotImplementedError ()
+		
+
+class Player(MovingObj):
     """Player(program_path, team, number, loc) -> create object to run
     and interface with player program"""
     def __init__(self, ppath, team, number, init_loc):
@@ -51,4 +58,43 @@ class Player:
         pass
 
     def move(self, x, y):
-        pass
+        pos+=vel
+	length = self.ground.length
+	width = self.ground.width
+	if y>(length/2) :
+		y=length/2
+	if y<-(length/2) :
+		y=-length/2
+	if x>(width/2) :
+		x=width/2
+	if x<-(width/2) :
+		x=-width/2
+class Ground :
+	def __init__( self  , length = 0 , width =0 , friction = 0 ) :
+		self.length = length
+		self.width = width
+		self.friction = Vector(friction ,friction )
+
+class Ball (MovingObj) :
+	def move ( self ):
+		m+=v
+		m+=ground.friction
+		if vx<0 :
+			vx=0
+		if vy<0 :
+			vy=0
+		length = self.ground.length
+		width = self.ground.width
+		if y>(length/2) :
+			self.vy= -vy
+			self.y= length-y
+		if x>(width/2) :
+			self.vx= -vx
+			self.x= width-x
+		if y<(-(length/2)) :
+			self.y= -length-y
+			self.vy=-vy
+		if x<-(width/2) :
+			self.vx= -vx
+			self.x= -width-x
+
--- engine/run_simulation.py
+++ engine/run_simulation.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python
+import sys
+import state
+from objects import Ground ,Ball , Player
+from geometry import Vector
+import state
+
+gwidth = 90
+glength = 120
+gfriction = 1
+
+indexi = [0 , 1/3 , -1/3 , 2/3 , -2/3]
+indexj = [1 , 2/3 , 2/3 , 1/3 , 1/3 ]
+
+if __name__ =='__main__':
+	ppath = sys.argv
+	progs = [sys.argv[1], sys.argv[2]]
+	ground = Ground(glength , gwidth ,gfriction )
+	#State(ppath)
+	players=[]
+	for i in range(2):
+			for j in range(5):
+				players.append(Player( progs[i] , i+1 , j+1 , Vector( indexi[j]*gwidth/2  ,  indexj[j]*glength/2 *(-1)**(i) ) ))
+	ball = Ball()
+	State( players , ball )
