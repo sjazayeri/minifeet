4b66c0559df5320110ce329cad2f42c75bf322a2
Mohammad Sadegh Jazayeri <sjazayeri404@gmail.com>
2014-10-14 01:49:56 +0330

    fixed indentation issues

--- engine/objects.py
+++ engine/objects.py
@@ -7,16 +7,16 @@ max_read = 1024 #this is likely to change as soon as the state specs are in
 # test
 
 class MovingObj:
-	def __init__ (self , pos=Vector(0, 0) , vel = 0, angel = 0 ,   ground=None ) :
-		self.ground = ground
-		self.pos=pos
-		self.vel=vel
-		self.angle=angel
-	def move (self) :
-		raise NotImplementedError ()
-	def distance_of(self,other):
-                return ((self.pos.x-other.pos.x)**2+(self.pos.y-other.pos.y)**2)**(.5)
-		
+    def __init__ (self , pos=Vector(0, 0) , vel = 0, angel = 0 ,   ground=None ) :
+        self.ground = ground
+        self.pos=pos
+        self.vel=vel
+        self.angle=angel
+    def move (self) :
+        raise NotImplementedError ()
+    def distance_of(self,other):
+        return ((self.pos.x-other.pos.x)**2+(self.pos.y-other.pos.y)**2)**(.5)
+        
 
 class Player(MovingObj):
     """Player(program_path, team, number, loc) -> create object to run
@@ -64,42 +64,42 @@ class Player(MovingObj):
 
     def move(self, x, y):
         pos+=vel
-	length = self.ground.length
-	width = self.ground.width
-	if y>(length/2) :
-		y=length/2
-	if y<-(length/2) :
-		y=-length/2
-	if x>(width/2) :
-		x=width/2
-	if x<-(width/2) :
-		x=-width/2
+        length = self.ground.length
+        width = self.ground.width
+        if y>(length/2) :
+            y=length/2
+        if y<-(length/2) :
+            y=-length/2
+        if x>(width/2) :
+            x=width/2
+        if x<-(width/2) :
+            x=-width/2
 class Ground :
-	def __init__( self  , length = 0 , width =0 , friction = 0 ) :
-		self.length = length
-		self.width = width
-		self.friction = Vector(friction ,friction )
+    def __init__( self  , length = 0 , width =0 , friction = 0 ) :
+        self.length = length
+        self.width = width
+        self.friction = Vector(friction ,friction )
 
 class Ball (MovingObj) :
-	def move ( self ):
-		m+=v
-		m+=ground.friction
-		if vx<0 :
-			vx=0
-		if vy<0 :
-			vy=0
-		length = self.ground.length
-		width = self.ground.width
-		if y>(length/2) :
-			self.vy= -vy
-			self.y= length-y
-		if x>(width/2) :
-			self.vx= -vx
-			self.x= width-x
-		if y<(-(length/2)) :
-			self.y= -length-y
-			self.vy=-vy
-		if x<-(width/2) :
-			self.vx= -vx
-			self.x= -width-x
+    def move ( self ):
+        m+=v
+        m+=ground.friction
+        if vx<0 :
+            vx=0
+        if vy<0 :
+            vy=0
+        length = self.ground.length
+        width = self.ground.width
+        if y>(length/2) :
+            self.vy= -vy
+            self.y= length-y
+        if x>(width/2) :
+            self.vx= -vx
+            self.x= width-x
+        if y<(-(length/2)) :
+            self.y= -length-y
+            self.vy=-vy
+        if x<-(width/2) :
+            self.vx= -vx
+            self.x= -width-x
 
--- engine/run_simulation.py
+++ engine/run_simulation.py
@@ -13,13 +13,13 @@ indexi = [0 , 1/3 , -1/3 , 2/3 , -2/3]
 indexj = [1 , 2/3 , 2/3 , 1/3 , 1/3 ]
 
 if __name__ =='__main__':
-	ppath = sys.argv
-	progs = [sys.argv[1], sys.argv[2]]
-	ground = Ground(glength , gwidth ,gfriction )
-	players=[]
-	for i in range(2):
-			for j in range(5):
-				players.append(Player( progs[i] , i+1 , j+1 , Vector( indexi[j]*gwidth/2  ,  indexj[j]*glength/2 *(-1)**(i) ) ))
-	ball = Ball()
-	state = State( players , ball )
-	state.send_data()
+    ppath = sys.argv
+    progs = [sys.argv[1], sys.argv[2]]
+    ground = Ground(glength , gwidth ,gfriction )
+    players=[]
+    for i in range(2):
+            for j in range(5):
+                players.append(Player( progs[i] , i+1 , j+1 , Vector( indexi[j]*gwidth/2  ,  indexj[j]*glength/2 *(-1)**(i) ) ))
+    ball = Ball()
+    state = State( players , ball )
+    state.send_data()
