d691644a05486a229fa077b42374a95b17be3a47
Mohammad Sadegh Jazayeri <sjazayeri404@gmail.com>
2014-10-17 11:35:34 +0330

    split proxy functionality from player into own class

--- engine/objects.py
+++ engine/objects.py
@@ -3,9 +3,6 @@ from geometry import Vector
 import subprocess as sp
 import select
 
-max_read = 1024 #this is likely to change as soon as the state specs are in
-# test
-
 class MovingObj:
     def __init__ (self , pos=Vector(0, 0) , vel = 0, angel = 0 ,   ground=None ) :
         self.ground = ground
@@ -22,45 +19,7 @@ class Player(MovingObj):
     """Player(program_path, team, number, loc) -> create object to run
     and interface with player program"""
     def __init__(self, ppath, team, number, init_loc):
-        self.team = team
-        self.number = number
-        self.loc = init_loc
-        self.goal = 'nop'
-        self.dic = defaultdict(lambda:lambda:'nop',
-                               {'kick': self.kick, 'move': self.move})
-        
-        self.process = sp.Popen(ppath, stdin=sp.PIPE, stdout=sp.PIPE)
-        self.poller = select.poll()
-        self.poller.register(self.process.stdout, select.POLLIN)
-        self.process.stdin.write(' '.join(map(str, [team, number, x, y, '\n'])))
-        
-    def terminate(self):
-        """terminates the player process"""
-        self.process.terminate()
-        
-    def send_state(self, state):
-        """sends the current state to the player"""
-        pass
-
-    def get_command(self):
-        """returns the command for the current cycle as a string"""
-        buffer_content = []
-        while self.poller.poll(0) and len(command) < max_read:
-            buffer_content += self.process.stdout.read(1)
-        command = ''.join(buffer_content)
-        if command:
-            self.goal = command.split('\n')[-1]
-        return self.translate(self.goal)
-        
-    def translate(self, goal):
-        args = goal.split(' ')
-        try:
-            return self.dic[args[0]](*map(int, args[1:]))
-        except TypeError:
-            return 'nop'
-        
-    def kick(self, x, y):
-        pass
+        pass        
 
     def move(self, x, y):
         pos+=vel
@@ -74,12 +33,15 @@ class Player(MovingObj):
             x=width/2
         if x<-(width/2) :
             x=-width/2
+
+            
 class Ground :
     def __init__( self  , length = 0 , width =0 , friction = 0 ) :
         self.length = length
         self.width = width
         self.friction = Vector(friction ,friction )
 
+        
 class Ball (MovingObj) :
     def move ( self ):
         m+=v
