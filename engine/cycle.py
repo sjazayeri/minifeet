986aa541da8311034cc64fd36b6d7557f7012e48
Feri73 <ferisampad@gmail.com>
2014-10-18 20:06:51 +0330

    static -> classis

--- engine/cycle.py
+++ engine/cycle.py
@@ -5,32 +5,26 @@ from geometry import Vector
 from objects import Ball , Player
 from sys import stderr
 
-"""a static class for performing each cycle tasks"""
 class Cycle:
     def __init__(self):
         self.commands = {'kick':self._kick, 'move':self._move}
-        self._players = []
         self._state = None
-        # _ball=None
         self.MaxBallDis = 10
         self.BallVelUnit = 100
         self.PlayerVel = 20
     """parses commands of players and updates
     the state of them and the ball"""
-    def update_players(self, players,ball,state):
-        Cycle._players=players
-        #Cycle._ball=ball
+    def update_players(self,state):
+        self._state=state
         state.last_kicked=None
-        _state=state
-        #print >>stderr, players
-        for p in players:
+        for p in state.players:
             ss=p.comm.get_command().split(' ')
             self.commands[ss[0]](p,*map(int, ss[1:]))
-    def _kick(p,angle,strength):
-        if len(p.pos-ball.pos) <= Cycle.MaxBallDis:
-            state.last_kicked=p
-            ball.vel=Cycle.BallVelUnit*strength
-            ball.angel=angle
-    def _move(p,angle,distance):
-        vel=min(distance,Cycle.PlayerVel)
-        p.vel=Vector(vel*math.cos(angel),vel*math.sin(angel))#
+    def _kick(self,p,angle,strength):
+        if len(p.pos-ball.pos) <= self.MaxBallDis:
+            _state.last_kicked=p
+            _state.ball.vel=self.BallVelUnit*strength
+            _state.ball.angel=angle
+    def _move(self,p,angle,distance):
+        vel=min(distance,self.PlayerVel)
+        p.vel=Vector(vel*math.cos(angel),vel*math.sin(angel))
--- engine/referee.py
+++ engine/referee.py
@@ -1,18 +1,16 @@
 class Referee:
 	goal_1, goal_2 = 0, 0
-	
-	@staticmethod
-	def is_goal(ball, ground):
+	def is_goal(self,ball, ground):
 		y1 = ball.pos.y - ball.vel.y
 		x1 = ball.pos.x - ball.vel.x
 		m = 1.0*(ball.pos.y-y1)/(ball.pos.x-x1)
 		d = y1 - m * x1
 		
 		if ball.pos.y > ground.length/2.0 and (ground.length/2.0 + m*ground.gate/2.0 - d)*(ground.length/2.0 - m*ground.gate/2.0 - d) < 0:
-			Referee.goal_1 += 1
+			self.goal_1 += 1
 			return 1
 		elif ball.pos.y < -(ground.length/2.0) and (-ground.length/2.0 + m*ground.gate/2.0 - d)*(-ground.length/2.0 - m*ground.gate/2.0 - d) < 0:
-			Referee.goal_2 += 1
+			self.goal_2 += 1
 			return 2
 		else:
 			return 0
