8b07d023abf584e75b8727f8b5ed419a60192d07
Mohammad Sadegh Jazayeri <sjazayeri404@gmail.com>
2014-10-24 19:57:17 +0330

    collision resolution in python base

--- engine/sample_player.py
+++ engine/sample_player.py
@@ -8,10 +8,13 @@ from random import random
 
 class Player():
     def run(self):
+        self.last_move_pos = Vector(0, 0)
         self.team, self.number = map(int, raw_input().split(' '))
         while True:
             players = get_players(self.team)
             selfpos = players[5 * self.team + self.number]
+            self.self_pos = selfpos
+            self._players = players
             ballpos = get_ballpos(self.team)
             game_state = int(raw_input())
             # print >>stderr, 'GOT DATA AT %f'%(time.time())
@@ -47,8 +50,12 @@ class Player():
         if self.team == 0:
             position.x *= -1
             position.y *= -1
+        if (self.last_move_pos-position).len() < 0.3 and self.nearest_opponent_dist() < 2:
+            vec = Vector(-2 + int(random()*2) * 2, -2 + int(random() * 2) * 2)
+            position = self.self_pos + vec
         print 'move %f %f' % (position.x, position.y)
         stdout.flush()
+        self.last_move_pos = position
 
     def kick(self, target, power):
         if self.team == 0:
@@ -56,6 +63,15 @@ class Player():
             target.y *= -1
         print 'kick %f %f %f' % (target.x, target.y, power)
         stdout.flush()
+
+    def nearest_opponent_dist(self):
+        opp = 1 - self.team
+        res = 10000
+        for i in range(0, 5):
+            p = self._players[5*opp+i]
+            if (p-self.self_pos).len() < res:
+                res = (p-self.self_pos).len()
+        return res
 
     def get_ball_receiver(self, players, ballpos):
         min_dist = 10000
